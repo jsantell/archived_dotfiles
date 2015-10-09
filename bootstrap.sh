36c4cbae0edcedadc941bd2d9b4fac4979241f67
Jordan Santell <jordan@jsantell.com>
2015-10-09 13:35:01 -0700

    Update bootstrap to pull and sync with home dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,11 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
+git pull origin master;
+
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "sync.sh" \
-    -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
